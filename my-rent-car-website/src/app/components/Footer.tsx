139fdaa3e70e8812fcbbca50836068863cd399bb
A-Sami-giaic <a.samad.khan009@gmail.com>
2024-12-09 22:46:01 +0500

    Update Footer.tsx

--- my-rent-car-website/src/app/components/Footer.tsx
+++ my-rent-car-website/src/app/components/Footer.tsx
@@ -11,42 +11,37 @@ export default function Footer() {
             Our vision is to provide convenience and help increase your sales business.
           </p>
         </div>
-
-   
         <div className="lists flex flex-wrap gap-6 justify-center lg:justify-between w-full lg:w-auto lg:mr-10">
-          <div className="about">
-            <ul className="flex flex-col gap-1">
+        <div className="about">
+       <ul className="flex flex-col gap-1">
               <li className="font-bold text-lg"><h1>About</h1></li>
               <li>How it works</li>
               <li>Featured</li>
               <li>Partnership</li>
               <li>Business Relation</li>
-            </ul>
-          </div>
-          <div className="community">
-            <ul className="flex flex-col gap-1">
+      </ul>
+      </div>
+      <div className="community">
+       <ul className="flex flex-col gap-1">
               <li className="font-bold text-lg"><h1>Community</h1></li>
               <li>Events</li>
               <li>Blog</li>
               <li>Podcast</li>
               <li>Invite a friend</li>
-            </ul>
-          </div>
-          <div className="socials">
-            <ul className="flex flex-col gap-1">
+      </ul>
+      </div>
+       <div className="socials">
+      <ul className="flex flex-col gap-1">
               <li className="font-bold text-lg"><h1>Socials</h1></li>
               <li>Discord</li>
               <li>Instagram</li>
               <li>Facebook</li>
               <li>Twitter</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-
- 
+       </ul>
+       </div>
+    </div>
+    </div>
       <div className="line border-t w-full border-[#e7eef6]"></div>
-
       <div className="last w-full flex flex-wrap items-center justify-between gap-4">
         <div className="first text-center lg:text-left w-full lg:w-auto">
           <h1 className="font-bold text-sm">©2022 MORENT. All rights reserved</h1>
@@ -54,8 +49,8 @@ export default function Footer() {
         <div className="second flex flex-wrap justify-center lg:justify-end items-center gap-4 w-full lg:w-auto">
           <h1 className="font-bold text-sm">Privacy & Policy</h1>
           <h1 className="font-bold text-sm">Terms & Conditions</h1>
-        </div>
-      </div>
-    </div>
+ </div>
+ </div>
+ </div>
   );
 }
